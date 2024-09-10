a0ee945b2283b017692a20dd040e1f97634f03a9
Карачев Кирилл <umbrellamixmail@gmail.com>
2024-09-10 17:02:50 +0700

    Обновление пользователей

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -1,12 +1,68 @@
-import uuid
+import uuid, jwt, datetime
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 
-class User(models.Model):
+class UserManager(BaseUserManager):
+    """Наследование логики с модели пользователя Django для кастомного пользователя"""
+
+    def create_user(self, username, email, password=None):
+
+        if username is None:
+            raise TypeError("User не имеет имени")
+        
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+
+        return user
+    
+    def create_superuser(self, username, email, password):
+        """ Создает и возввращет пользователя с привилегиями суперадмина. """
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Основная форма для пользователя, унаследованная от Django User"""
     id = models.AutoField(primary_key=True)
-    user_id = models.UUIDField(null=False, editable=False, default=uuid.uuid4)
-    username = models.CharField(max_length=20, null=False, default=None)
-    email = models.EmailField(null=False)
-    userage = models.PositiveIntegerField(null=False, default=0)
+    user_id = models.UUIDField(default=uuid.uuid4)
+
+    email = models.EmailField(db_index=True, unique=True)
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    password = models.TextField()
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
 
     def __str__(self):
-        return self.name
+        return self.username
+    
+    @property
+    def token(self):
+        """Получение JWT токена путем вызова user.token"""
+        return self._generate_jwt_token()
+    
+    def _generate_jwt_token(self):
+
+        dt = datetime.datetime.now() + datetime.timedelta(days=1)
+
+        token = jwt.encode({
+            'id': self.pk,
+            'exp': int(dt.strftime('%s'))
+        }, settings.SECRET_KEY, algorithm='HS256')
+
+        return token.decode('utf-8')
